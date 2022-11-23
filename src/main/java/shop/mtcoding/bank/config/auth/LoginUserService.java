2e83e2a495e084f87cde2ad2c605cbfc08a432cb
hyonna12 <hyonna01@gmail.com>
2022-11-23 14:30:29 +0900

    security - login

--- src/main/java/shop/mtcoding/bank/config/auth/LoginUserService.java
+++ src/main/java/shop/mtcoding/bank/config/auth/LoginUserService.java
@@ -1,11 +1,14 @@
 package shop.mtcoding.bank.config.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
 
 @Service
@@ -18,7 +21,13 @@ public class LoginUserService implements UserDetailsService {
   // userdetails를 implement한 user를 넣어서 findById를 넣어주면 검증해서 로그인 됨
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return null;
+
+    // LoginUser loginUser = (LoginUser)
+    // SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new RuntimeException("username을 찾을 수 없습니다."));
+    return new LoginUser(user);
   }
 
 }
--- src/main/java/shop/mtcoding/bank/domain/user/UserRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/user/UserRepository.java
@@ -1,7 +1,14 @@
 package shop.mtcoding.bank.domain.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  @Query("select u from User u where usernmae = :username") // jpql - 객체로 조회
+  Optional<User> findByUsername(@Param("username") String username);
 
 }
