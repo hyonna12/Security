175eed6048e9ad34aa29ef2b3aec74b297d13661
hyonna12 <hyonna01@gmail.com>
2022-11-24 14:34:50 +0900

    남은일 정리

--- src/main/java/shop/mtcoding/bank/web/UserApiController.java
+++ src/main/java/shop/mtcoding/bank/web/UserApiController.java
@@ -2,12 +2,15 @@ package shop.mtcoding.bank.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.bank.config.auth.LoginUser;
 import shop.mtcoding.bank.dto.ResponseDto;
 import shop.mtcoding.bank.dto.UserReqDto.JoinReqDto;
 import shop.mtcoding.bank.dto.UserRespDto.JoinRespDto;
@@ -19,6 +22,11 @@ import shop.mtcoding.bank.service.UserService;
 public class UserApiController {
 
   private final UserService userService;
+
+  // @GetMapping("/user/session")
+  // public String userSession(@AuthenticationPrincipal LoginUser loginUser) {
+  // return "username : " + loginUser.getUsername();
+  // } // 페이지 들어가려면 인증 필요한데 세션값을 넣어줌
 
   @PostMapping("/join")
   public ResponseEntity<?> join(@RequestBody JoinReqDto joinReqDto) {
